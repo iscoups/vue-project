63d5d653bb4955c197bdd2f5426c1b4894694bc4
iscoups <2572209130@qq.com>
2023-07-05 21:44:56 +0800

    feat:吸顶导航交互事件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="height:1200px">
     Home
   </div>
 </template>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,13 @@
 <script setup>
+import LayoutFixed from './components/LayoutFixed.vue'
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixedVue from './components/LayoutFixed.vue'
 </script>
 
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
