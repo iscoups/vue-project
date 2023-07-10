0781f3f4511254ba248d4142a661abb60fc39106
iscoups <2572209130@qq.com>
2023-07-10 21:26:37 +0800

    feat：home页面实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,31 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+</script>
+
 <template>
-  <div style="height:1200px">
-    Home
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
   </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
+  <!-- 面板部分 -->
+  <HomePanel mainTitle="新鲜好物"
+             subTitle="好物好物好物">
+    <div>
+      这里是新鲜好物
+    </div>
+  </HomePanel>
+  <HomePanel mainTitle="人气推荐"
+             subTitle="推荐推荐推荐">
+    <div>
+      这里是推荐
+    </div>
+  </HomePanel>
 </template>
